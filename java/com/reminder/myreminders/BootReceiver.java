af5028631d14cbf820ae829f32e40994ae5c238f
Manjiro <85405446+grocerystoreproject@users.noreply.github.com>
2025-10-09 19:28:49 +0530

    Create BootReceiver.java

---
+++ java/com/reminder/myreminders/BootReceiver.java
@@ -0,0 +1,22 @@
+package com.reminder.myreminders;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    
+    private static final String TAG = "MyReminders";
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action) || 
+            "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
+            
+            Log.d(TAG, "Device booted - alarms need rescheduling");
+        }
+    }
+}
